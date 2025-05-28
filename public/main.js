aa328dfda7465c6474a546f8fc9180f8d64199c0
GouravIngle <gouravingle1874@gmail.com>
2025-05-28 19:05:17 +0530

    init commit

--- public/main.js
+++ public/main.js
@@ -1,16 +1,23 @@
 const form = document.getElementById("sql-form");
 const output = document.getElementById("output");
 
-    form.onsubmit = async (e) => {
-      e.preventDefault();
-      const prompt = document.getElementById("prompt").value;
+form.onsubmit = async (e) => {
+  e.preventDefault();
+  const prompt = document.getElementById("prompt").value;
 
-      const res = await fetch("/generate-sql", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ prompt }),
-      });
+  const res = await fetch("/generate-sql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ prompt }),
+  });
 
-      const data = await res.json();
-      output.textContent = data.query|| data.error ||  "❌ Error generating query";
-    };
+  const data = await res.json();
+  output.textContent = data.query || data.error || "❌ Error generating query";
+};
+
+document.getElementById("copybtn").addEventListener("click", function () {
+  const output = document.getElementById("output").innerText;
+
+  navigator.clipboard
+    .writeText(output)
+});
